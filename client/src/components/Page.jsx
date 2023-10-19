60398f098758beedb12a1a472372d716c691cd4b
midhunrajk <midhunrajk009@gmail.com>
2023-10-19 18:30:56 +0530

    version 1.1 completed

--- client/src/components/Page.jsx
+++ client/src/components/Page.jsx
@@ -3,125 +3,99 @@
 import React, { useState } from "react";
 
 function Page() {
-    const greenArray = [
-        {
-            id: 1,
-            name: "text 1",
-        },
-        {
-            id: 2,
-            name: "text 2",
-        },
-        {
-            id: 3,
-            name: "text 3",
-        },
-        {
-            id: 4,
-            name: "text 4",
-        },
-    ];
-    const redArray = [
-        {
-            id: 5,
-            name: "text 5",
-        },
-        {
-            id: 6,
-            name: "text 6",
-        },
-        {
-            id: 7,
-            name: "text 7",
-        },
-        {
-            id: 8,
-            name: "text 8",
-        },
-  ];
-  const [greenItems, setGreenItems] = useState(greenArray);
-  const [redItems, setRedItems] = useState(redArray);
-  const [greenInput, setGreenInput] = useState("");
-  const [redInput, setRedInput] = useState("");
+    // Define initial items without "id" field
+    const initialGreenArray = [];
+    const initialRedArray = [];
 
-    const toRed = () => {
-        const selectedItems = greenItems.filter((item) => item.isSelected);
-        setGreenItems(greenItems.filter((item) => !item.isSelected));
-        setRedItems([...redItems, ...selectedItems.map((item) => ({ ...item, isSelected: false }))]);
+    // Use a single state for both green and red items
+    const [items, setItems] = useState({
+        green: initialGreenArray.map((name) => ({ name, isSelected: false })),
+        red: initialRedArray.map((name) => ({ name, isSelected: false })),
+    });
+
+    // Use a single input state for both green and red
+    const [input, setInput] = useState({
+        green: "",
+        red: "",
+    });
+
+    const toOpposite = (color) => {
+        const selectedItems = items[color].filter((item) => item.isSelected);
+        setItems((prevItems) => ({
+            ...prevItems,
+            [color]: prevItems[color].filter((item) => !item.isSelected),
+            [getOppositeColor(color)]: [
+                ...prevItems[getOppositeColor(color)],
+                ...selectedItems.map((item) => ({ ...item, isSelected: false })),
+            ],
+        }));
     };
 
-    const toGreen = () => {
-        const selectedItems = redItems.filter((item) => item.isSelected);
-        setRedItems(redItems.filter((item) => !item.isSelected));
-        setGreenItems([...greenItems, ...selectedItems.map((item) => ({ ...item, isSelected: false }))]);
+    const toggleSelection = (color, index) => {
+        setItems((prevItems) => ({
+            ...prevItems,
+            [color]: prevItems[color].map((item, i) =>
+                i === index ? { ...item, isSelected: !item.isSelected } : item
+            ),
+        }));
     };
 
-    const toggleSelection = (id, color) => {
-        const updateItems = (items) =>
-        items.map((item) => (item.id === id ? { ...item, isSelected: !item.isSelected } : item));
-        setGreenItems((prevItems) => (color === "green" ? updateItems(prevItems) : prevItems));
-        setRedItems((prevItems) => (color === "red" ? updateItems(prevItems) : prevItems));
-    };
-
-
-    const handleGreenInput = () => {
-        if(greenInput.trim() !== ""){
-            const newGreenItem = {
-                id: greenItems.length + 1,
-                name: greenInput.trim()
-            }
-            setGreenItems([...greenItems, newGreenItem]);
-            setGreenInput("");
+    const handleInput = (color) => {
+        if (input[color].trim() !== "") {
+            setItems((prevItems) => ({
+                ...prevItems,
+                [color]: [
+                    ...prevItems[color],
+                    { name: input[color].trim(), isSelected: false },
+                ],
+            }));
+            setInput((prevInput) => ({ ...prevInput, [color]: "" }));
         }
-    }
+    };
 
-    const handleRedInput = () => {
-        if(redInput.trim() !== ""){
-            const newRedItem = {
-                id: greenItems.length + redItems.length + 1,
-                name: redInput.trim()
-            }
-            setRedItems([...redItems, newRedItem]);
-            setRedInput("");
-        }
-    }
-  
+    const getOppositeColor = (color) => (color === "green" ? "red" : "green");
+
     return (
         <div className="flex justify-between h-[calc(100vh-40px)] m-5 p-4 bg-gray-100 rounded-xl">
+            {/* Green Column */}
             <div className="flex flex-col gap-5 w-5/12 p-10 bg-green-300 rounded-lg">
-                <ul className="text-xl">
                 <div className="flex gap-5">
-                    <input className="pl-2 rounded-lg focus:outline-none" type="text" placeholder="enter something..." value={greenInput} onChange={(e) => setGreenInput(e.target.value)}/>
-                    <button className="bg-green-200 p-2 rounded-lg hover:bg-green-100" onClick={handleGreenInput}>Add</button>
+                    <input className="pl-2 rounded-lg focus:outline-none" type="text" placeholder="enter something..." value={input.green} onChange={(e) => setInput({ ...input, green: e.target.value })}/>
+                    <button className="bg-green-200 p-2 rounded-lg hover:bg-green-100" onClick={() => handleInput("green")} >Add</button>
                 </div>
-                {greenItems.map((greenItem) => (
-                    <li className="flex m-1 gap-1 mt-5" key={greenItem.id} onClick={() => toggleSelection(greenItem.id, "green")}>
-                        <input type="checkbox" checked={greenItem.isSelected} readOnly />
-                        <h3>{greenItem.name}</h3>
-                    </li>
-                ))}
+                <ul className="text-xl">
+                    {items.green.map((greenItem, index) => (
+                        <li className="flex m-1 gap-1 mt-5" key={index} onClick={() => toggleSelection("green", index)} >
+                            <input type="checkbox" checked={greenItem.isSelected} readOnly />
+                            <h3>{greenItem.name}</h3>
+                        </li>
+                    ))}
                 </ul>
             </div>
+
+            {/* Middle Buttons */}
             <div className="flex w-1/12 flex-col justify-center p-1 gap-10">
-                <button onClick={toRed} className="flex justify-center bg-red-300 rounded-md py-2 px-6 hover:bg-red-400 active:bg-red-300">
-                    <img className="w-10" src={"/assets/icons/right-arrow.png"} alt="right"/>
+                <button onClick={() => toOpposite("green")} className="flex justify-center bg-red-300 rounded-md py-2 px-6 hover:bg-red-400 active:bg-red-300" >
+                    <img className="w-10" src={"/assets/icons/right-arrow.png"} alt="right" />
                 </button>
-                <button onClick={toGreen} className="flex justify-center bg-green-300 rounded-md py-2 px-6 hover:bg-green-400 active:bg-green-300">
-                    <img className="w-10" src={"/assets/icons/left-arrow.png"} alt="left"/>
+                <button onClick={() => toOpposite("red")} className="flex justify-center bg-green-300 rounded-md py-2 px-6 hover:bg-green-400 active:bg-green-300" >
+                    <img className="w-10" src={"/assets/icons/left-arrow.png"} alt="left" />
                 </button>
             </div>
+
+            {/* Red Column */}
             <div className="flex flex-col gap-5 w-5/12 p-10 bg-red-300 rounded-lg">
                 <div className="flex gap-5">
-                    <input className="pl-2 rounded-lg focus:outline-none" type="text" placeholder="enter something..." value={redInput} onChange={(e) => setRedInput(e.target.value)}/>
-                    <button className="bg-red-200 p-2 rounded-lg hover:bg-red-100" onClick={handleRedInput}>Add</button>
+                    <input className="pl-2 rounded-lg focus:outline-none" type="text" placeholder="enter something..." value={input.red} onChange={(e) => setInput({ ...input, red: e.target.value })}/>
+                    <button className="bg-red-200 p-2 rounded-lg hover:bg-red-100" onClick={() => handleInput("red")}>Add</button>
                 </div>
                 <ul className="text-xl">
-                {redItems.map((redItem) => (
-                    <li className="flex m-1 gap-1 mt-5" key={redItem.id} onClick={() => toggleSelection(redItem.id, "red")}>
-                        <input type="checkbox" checked={redItem.isSelected} readOnly />
-                        <h3>{redItem.name}</h3>
-                    </li>
-                ))}
+                    {items.red.map((redItem, index) => (
+                        <li className="flex m-1 gap-1 mt-5" key={index} onClick={() => toggleSelection("red", index)} >
+                            <input type="checkbox" checked={redItem.isSelected} readOnly />
+                            <h3>{redItem.name}</h3>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
