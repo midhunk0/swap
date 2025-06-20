4c8257dbb22972648d46de6d06b58a98d75f61b6
midhunk0 <midhunrajk009@gmail.com>
2025-06-20 18:20:32 +0530

    removed unwated files and folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
-import './App.css'
 import Page from './components/Page'
-import tailwindConfig from '../tailwind.config'
 
 function App() {
 
